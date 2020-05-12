4113e6710123aff8218ef40438fec9c763155bfe
Tom McDonald <tommcdon@hotmail.com>
2020-05-12 02:45:41 -0700

    Add VS 2017 15.9 JavaScript to C# interop samples

---
+++ JS-CSharp-V2/JS-CSharp-V2/js/main.js
@@ -0,0 +1,16 @@
+﻿// Your code here!
+"use strict";
+
+window.onload = function () {
+    document.getElementById("helloButton").onclick = function (eventInfo) {
+        var userName = document.getElementById("nameInput").value;
+        var greetingString = "Hello, " + userName + "!";
+
+        var t = new RuntimeComponent1.Class1();
+        var s = t.getFoo("foo");
+        console.debug(s);
+
+        greetingString += s;
+        document.getElementById("greetingOutput").innerText = greetingString;
+    }
+}
