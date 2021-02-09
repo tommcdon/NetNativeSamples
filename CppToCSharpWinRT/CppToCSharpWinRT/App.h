f7d8608d193261320909913fd2b35aeee083356f
Tom McDonald <tommcdon@microsoft.com>
2021-02-08 21:23:46 -0800

    Add CppToCSharpWinRT

---
+++ CppToCSharpWinRT/CppToCSharpWinRT/App.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "App.xaml.g.h"
+
+namespace winrt::CppToCSharpWinRT::implementation
+{
+    struct App : AppT<App>
+    {
+        App();
+
+        void OnLaunched(Windows::ApplicationModel::Activation::LaunchActivatedEventArgs const&);
+        void OnSuspending(IInspectable const&, Windows::ApplicationModel::SuspendingEventArgs const&);
+        void OnNavigationFailed(IInspectable const&, Windows::UI::Xaml::Navigation::NavigationFailedEventArgs const&);
+    };
+}
---
+++ CppToCSharpWinRT/SampleComponent/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleComponent
+{
+    public sealed class Example
+    {
+        int MyNumber;
+
+        public string GetMyString()
+        {
+            return $"This is call #: {++MyNumber}";
+        }
+    }
+}
