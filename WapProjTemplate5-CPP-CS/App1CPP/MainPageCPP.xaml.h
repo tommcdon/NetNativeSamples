a794e1f0bd2c3430d69d093ccd0a91fb9f33bd05
Tom McDonald <tommcdon@microsoft.com>
2024-10-22 17:02:01 -0400

    Add WAP UWP C++ and C# combination sample

---
+++ WapProjTemplate5-CPP-CS/App1CPP/MainPageCPP.xaml.h
@@ -0,0 +1,23 @@
+﻿//
+// MainPageCPP.xaml.h
+// Declaration of the MainPageCPP class.
+//
+
+#pragma once
+
+#include "MainPageCPP.g.h"
+
+namespace App1CPP
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public ref class MainPageCPP sealed
+	{
+	public:
+		MainPageCPP();
+
+	private:
+		void Button_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+	};
+}
